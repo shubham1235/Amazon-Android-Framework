ec781d87276056ffb6f2dd0c565fe469cd574601
shubham verma <shubhamverma815@gmail.com>
2019-10-05 12:54:32 +0530

    creating maven project
    creating maven project for Amazon App andoid automation,
    Mavne -: maven-archtype-quikestart
    
    Project Maven info
      <groupId>org.appium.android</groupId>
      <artifactId>AmazonShopping</artifactId>
      <version>0.0.1-SNAPSHOT</version>
      <packaging>jar</packaging>

---
+++ AmazonShopping/src/main/java/org/appium/android/AmazonShopping/App.java
@@ -0,0 +1,13 @@
+package org.appium.android.AmazonShopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
