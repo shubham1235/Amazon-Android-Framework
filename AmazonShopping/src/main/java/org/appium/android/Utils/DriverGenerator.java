f9da7e0405cf764505b3a7b03534b25384156da6
shubham verma <shubhamverma815@gmail.com>
2019-10-06 21:44:39 +0530

    Update DriverGenerator.java
    Remove main method which is used for Appium server testing

--- AmazonShopping/src/main/java/org/appium/android/Utils/DriverGenerator.java
+++ AmazonShopping/src/main/java/org/appium/android/Utils/DriverGenerator.java
@@ -9,30 +9,38 @@ import io.appium.java_client.android.AndroidDriver;
 
 public class DriverGenerator {
 	public static AndroidDriver<MobileElement> appiumDriver;
-	AppiumServer appiumServer = new AppiumServer();
 
-	public static void main(String[] args) throws Exception {
-		DriverGenerator driverGenerator = new DriverGenerator();
-		driverGenerator.connectToAppiumServer();
+	static AppiumServer appiumServer = new AppiumServer();
+
+
+	
+	/**
+	 * Get AndroidDriver
+	 * @return
+	 * @throws Exception 
+	 */
+	public static AndroidDriver<MobileElement> getAppiumDriver() throws Exception {	
+		return appiumDriver;
 	}
 
+	
 	/**
 	 * This method establishing the connection between framework and Appium server
 	 * which is invoke by the appiumServer.startAppiumServer()
 	 * 
 	 * @throws Exception
 	 */
-	public void connectToAppiumServer() throws Exception {
+	public static AndroidDriver<MobileElement> connectToAppiumServer() throws Exception {
 
 		// invoke appium server
-		appiumServer.startAppiumServer();
+		//appiumServer.startAppiumServer();
 		// wait for Appium server start internal
 		Thread.sleep(2000);
 		appiumDriver = new AndroidDriver<MobileElement>(
 				new URL("http://0.0.0.0:" + Helper.getProjectProperties("appiumPortNumber") + "/wd/hub"),
 				DeviceConfig.getDeviceCapability());
 		
-		appiumDriver.launchApp();
+		return appiumDriver;
 
 	}
 
