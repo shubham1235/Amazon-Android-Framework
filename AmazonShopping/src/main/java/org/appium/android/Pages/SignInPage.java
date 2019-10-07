0f831305b13fd976b95f6f01f2eb6f2cfc95ed5e
shubham verma <shubhamverma815@gmail.com>
2019-10-07 14:09:49 +0530

    Update SignInPage
    add comment for evey method,
    add verdiyWelcomeText :- which is verify welcome page welcome element

--- AmazonShopping/src/main/java/org/appium/android/Pages/SignInPage.java
+++ AmazonShopping/src/main/java/org/appium/android/Pages/SignInPage.java
@@ -34,58 +34,87 @@ public class SignInPage extends BaseUtil {
 
 	@FindBy(id = "skip_sign_in_buttonn")
 	public WebElement skip_Sign_In_Button;
+	
+	@FindBy(xpath="//android.view.View[contains(@resource-id,'outer-accordion-signin-signup-page')]//*[@text='Welcome']")
+	public WebElement welcomeLable;
+	
 
 	
-	@FindBy(id = "register_accordion_header")
-	public WebElement yoyo;
-	
-	
-	public void clickonyoyo()
-	{
-		clickOnElement(yoyo);
-		
-		System.out.println(yoyo.getText());
-		System.out.println(yoyo.isSelected());
-		System.out.println(yoyo.isDisplayed());
-	}
-	
+	/**
+	 * Get SignIn page Second Label element Text
+	 * @return
+	 */
 	public String getSignInPageText() {
 		return getElementText(signin_To_YourAccount_Label);
-
-	}
+		}
 	
-	public boolean isSignInElementIsEnable()
-	{
+	/**
+	 * Check SignIn label is enable of Disable
+	 * @return
+	 */
+	public boolean isSignInElementIsEnable(){
 		return isElementEnabled(signin_To_YourAccount_Label);
 		
 	}
 	
-	public String getViewYourWishListText()
-	{
+	/**
+	 * Get SignIn page Second Label Text
+	 * @return
+	 */
+	public String getViewYourWishListText(){
 		return getElementText(view_Your_Wish_List_Label);
 	}
 	
-	public String getFindPurchaseText()
-	{
+	/**
+	 * Get find Purchase label element Text
+	 * @return
+	 */
+	public String getFindPurchaseText()	{
 		return getElementText(find_Purchase_Label);
 	}
 	
 	
-	public String getTrackYourPackagesText()
-	{
+	/**
+	 * Get track your packages label Element Text
+	 * @return
+	 */
+	public String getTrackYourPackagesText(){
 		return getElementText(track_Your_Packages_Label);
 	}
-		
+	
+	
+	/**
+	 * click on SignIn Button
+	 * @return
+	 */	
 	public boolean clickOnSignInButton() {
 		return clickOnElement(sign_in_Button);
 	}
 	
+	
+	/**
+	 * Check SingIn Button is Enable or not
+	 * @return
+	 */
 	public boolean isSignInButtonIsEnable() {
 		return isElementEnabled(sign_in_Button);
 	}
 
+	/**
+	 * Get SignIn button Text
+	 * @return
+	 */
 	public String getSingInButtonText() {
 		return getElementText(sign_in_Button);
 
+	}
+	
+	/**
+	 * verify Welcome page Text from Welcome Page
+	 * @return
+	 */
+	public boolean verdiyWelcomeText() {
+	//	System.out.println(welcomeLable.getText());
+		return verifyTextIsVisible(welcomeLable,"Welcome");
 	}
 }
