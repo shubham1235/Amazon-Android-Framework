7b4cae093a891a663de5fe0b3798e28422d8f9e3
shubham verma <shubhamverma815@gmail.com>
2019-10-09 01:29:56 +0530

    Update SignInTest.java

--- AmazonShopping/src/main/java/org/appium/android/Tests/SignInTest.java
+++ AmazonShopping/src/main/java/org/appium/android/Tests/SignInTest.java
@@ -1,24 +1,26 @@
 package org.appium.android.Tests;
 
 import org.appium.android.Pages.SignInPage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class SignInTest extends SignInPage {	
-	SoftAssert softAssert;
-	
 	public SignInTest() throws Exception {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
+	SoftAssert softAssert;
 	
 	
 	/**
 	 * Test summary : Verify all The element Text and verify Click able element is disable or enable
+	 * @throws Exception 
 	 * @throws InterruptedException
 	 */
 	@Test(priority = 1)
-	public void verifySingInPageElement()
+	public void verifySingInPageElement() throws Exception
 	{
 		softAssert = new SoftAssert();		
 		softAssert.assertTrue(isSignInElementIsEnable(),"Sign in to your account label Not Enable");
